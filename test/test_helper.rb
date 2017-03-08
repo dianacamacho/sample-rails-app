3173dd62516c3013bec23dee9b424593e650ad66
dianacamacho <dcam1901@yahoo.com>
2017-03-08 17:34:00 -0600

    authorization in progress

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,7 +6,8 @@ Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
   fixtures :all
-
+  include ApplicationHelper
+  
   # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
